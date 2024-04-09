cc0aeed1709afcdafa44508c904d2877e5cd2a6e
owen.xiao <owen.xiao@anker-in.com>
2024-04-09 18:11:45 +0800

    feat:新增go文件

---
+++ util/retry_func.go
@@ -0,0 +1,16 @@
+package util
+
+// retryTask 重试任务
+func retryTask(handFunc func() error) error {
+	var (
+		err     error
+		retries = 3
+	)
+	for retries > 0 {
+		if err = handFunc(); err == nil {
+			break
+		}
+		retries--
+	}
+	return err
+}
